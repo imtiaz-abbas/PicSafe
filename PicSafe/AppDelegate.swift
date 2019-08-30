bf14d4369339da358cce3076a9a6cdca182abcd7
imtiaz191919 <imtiaz@uncommon.is>
2019-08-30 17:46:19 +0530

    changed AppDelegate to add navigationController

--- PicSafe/AppDelegate.swift
+++ PicSafe/AppDelegate.swift
@@ -13,10 +13,16 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  var navigationController: UINavigationController?
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.navigationController = UINavigationController(rootViewController: UIViewController())
+    navigationController?.isNavigationBarHidden = true
+    self.window?.rootViewController = navigationController
+    self.window?.makeKeyAndVisible()
     return true
   }
 
